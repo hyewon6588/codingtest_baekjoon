8ef45463a7284bfec1f22125157653408136904c
hw1010 <66793549+hyewon6588@users.noreply.github.com>
2023-09-30 23:21:21 -0400

    [Bronze V] Title: 사칙연산, Time: 212 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n1=s.nextInt();
+        int n2=s.nextInt();
+        System.out.println(n1+n2);
+        System.out.println(n1-n2);
+        System.out.println(n1*n2);
+        System.out.println(n1/n2);
+        System.out.println(n1%n2);
+    }
+}
